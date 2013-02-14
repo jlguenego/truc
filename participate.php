c038a8efaefa80a8cdf3385f3b4b0ca8b816d77f
THOMIAS Yannis <thomias.yannis@gmail.com>
2013-02-14 14:09:18 +0100

    Can now participate to an event. But not correct prices.

--- participate.php
+++ participate.php
@@ -13,12 +13,15 @@
 		$event = get_event($_GET["id"]);
 	}
 	if (!is_null_or_empty($event) && !is_null_or_empty($_POST['participate'])) {
+		if (!is_number($_POST['person_amount'])) {
+			$error_msg = "Please enter a number for the amount of person";
+		}
 		if (!isset($_POST['confirm'])) {
 			$error_msg = "You have to check the engagement";
 		}
 		if ($error_msg == "") {
 			$user = get_user_by_login($_SESSION['login']);
-			participate($user['id'], $event["id"], 1);
+			participate($user['id'], $event["id"], $_POST['person_amount']);
 			redirect_to("event.php?id=".$event["id"]);
 		}
 	}
@@ -30,11 +33,12 @@
 	<a href="index.php">Back to index</a><br/><br/>
 <?php
 	if ($error_msg != "") {
-		echo "$error_msg";
-	} else {
+		echo "$error_msg<br/><br/>";
+	}
 ?>
 	Are you sure you want to participate to this event?
 	<form name="input" action="participate.php?id=<?php echo $event['id']; ?>" method="POST">
+		Number of person: <input type="number" name="person_amount"/><br/>
 		<input type="checkbox" name="confirm"/> I engage myself to participate to this event.<br/>
 		<input type="hidden" name="participate" value="participate"/>
 		<input type="submit" value="Yes"/>		
@@ -43,7 +47,4 @@
 		<input type="hidden" name="confirm" value="no"/>
 		<input type="submit" value="No"/>		
 	</form>
-<?php
-	}
-?>
 </html>
