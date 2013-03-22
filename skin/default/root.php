634d6249a65d005d2bed7f1486df4208d052af14
GUENEGO Jean-Louis <jlguenego@gmail.com>
2013-03-22 10:51:33 +0100

    improving css

--- skin/default/root.php
+++ skin/default/root.php
@@ -1,16 +1,16 @@
+<div id="evt_main">
+	<div id="evt_menu">
+		<ul>
+			<li><a href="?action=get_form&amp;type=event">Create an event</a></li>
+			<li><a href="?action=retrieve&amp;type=events">See events</a></li>
 <?php
 	// Verify if user is logged in to show differents menu.
-	if (!is_logged()) {
+	if (is_logged()) {
 ?>
-	<a href="?action=retrieve&amp;type=events">See events</a>
-<?php
-	} else {
-?>
-	<a href="?action=retrieve&amp;type=account">Show Account</a><br/>
-	<a href="?action=get_form&amp;type=event">Create an event</a><br/>
-	<a href="?action=retrieve&amp;type=events">See events</a><br/>
-	<a href="?action=sign_out">Sign out<a/><br/>
-	<a href="?action=supervision">Events supervision</a>
+			<li><a href="?action=supervision">Events supervision</a></li>
 <?php
 	}
 ?>
+		</ul>
+	</div>
+</div>
