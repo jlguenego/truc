5d63836f665acbeef09599353d6436048c99e1b1
THOMIAS Yannis <thomias.yannis@gmail.com>
2013-03-30 13:00:30 +0100

    Finalized.

--- skin/default/event_form.php
+++ skin/default/event_form.php
@@ -21,9 +21,11 @@
 	$item = $f->add_text("Organizer name", "organizer_name", default_value("organizer_name", $event->organizer_name),
 		"The entity that is responsible for organizing the event.");
 	$item->other_attr = 'size="60" maxlength="255"';
-	$item = $f->add_number("Required funding", "funding_needed",
-		default_value("funding_needed", $event->funding_needed),
-		"Minimum amount of fund needed to organize the event. Please indicate 0 if no requirement.");
+	if (!$event->has_accountancy_activity()) {
+		$f->add_number("Required funding", "funding_needed",
+			default_value("funding_needed", $event->funding_needed),
+			"Minimum amount of fund needed to organize the event. Please indicate 0 if no requirement.");
+	}
 	$f->add_text("Event starting date", "happening_t",
 		default_value("happening_t", $event->happening_t),
 		"Date at which starts the event (Format: YYYY-MM-DD).");
