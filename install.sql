12b605f7c10f600a06d345599db2a5af8e4e0035
Jean-Louis GUENEGO <jlguenego@gmail.com>
2013-04-09 09:10:47 +0200

    database updated. 255 instead of 25.

--- install.sql
+++ install.sql
@@ -1,95 +1,103 @@
-#Ma base de données :
+#------------------------------------------------------------
+#        Script MySQL.
+#------------------------------------------------------------
+
 
 CREATE TABLE event(
-        id                Int NOT NULL ,
-        created_t         Varchar(25) NOT NULL ,
-        mod_t             Varchar(25) NOT NULL ,
-        title             Varchar(255) NOT NULL ,
-        organizer_name    Varchar(255) NOT NULL ,
-        open_t            Varchar(25) NOT NULL ,
-        confirmation_t    Varchar(25) NOT NULL ,
-        happening_t       Varchar(25) NOT NULL ,
-        funding_needed    Decimal (25,2) NOT NULL ,
-        funding_acquired  Decimal (25,2) NOT NULL ,
-        location          Varchar(255) NOT NULL ,
-        link              Varchar(255),
-        short_description Text NOT NULL ,
-        long_description  Text,
-        type              Int NOT NULL ,
-        status            Int NOT NULL ,
-        publish_flag      Int NOT NULL ,
-        flags             Int,
-        id_user           Int NOT NULL ,
-        PRIMARY KEY (id)
+        id                Int NOT NULL  ,
+        created_t         Varchar (25) NOT NULL  ,
+        mod_t             Varchar (25) NOT NULL  ,
+        title             Varchar (255) NOT NULL  ,
+        organizer_name    Varchar (255) NOT NULL  ,
+        open_t            Varchar (25) NOT NULL  ,
+        confirmation_t    Varchar (25) NOT NULL  ,
+        happening_t       Varchar (25) NOT NULL  ,
+        funding_needed    Decimal (25,2) NOT NULL  ,
+        funding_acquired  Decimal (25,2) NOT NULL  ,
+        location          Varchar (255) NOT NULL  ,
+        link              Varchar (255) ,
+        short_description Text NOT NULL  ,
+        long_description  Text ,
+        type              Int NOT NULL  ,
+        status            Int NOT NULL  ,
+        publish_flag      Int NOT NULL  ,
+        flags             Int ,
+        id_user           Int NOT NULL  ,
+        PRIMARY KEY (id )
 )ENGINE=InnoDB;
+
 
 CREATE TABLE user(
-        id                Int NOT NULL ,
-        created_t         Varchar(25) NOT NULL ,
-        mod_t             Varchar(25) NOT NULL ,
-        email             Varchar(25) NOT NULL ,
-        password          Varchar(120) NOT NULL ,
-        lastname          Varchar(25) NOT NULL ,
-        firstname         Varchar(25) NOT NULL ,
-        role              Int NOT NULL ,
-        activation_status Int NOT NULL ,
-        activation_key    Varchar(255),
-        street            Varchar(255),
-        zip               Varchar(255),
-        city              Varchar(255),
-        country           Varchar(255),
-        state             Varchar(255),
-        locale            Varchar(25),
-        token             Varchar(255),
-        PRIMARY KEY (id)
+        id                Int NOT NULL  ,
+        created_t         Varchar (25) NOT NULL  ,
+        mod_t             Varchar (25) NOT NULL  ,
+        email             Varchar (255) NOT NULL  ,
+        password          Varchar (255) NOT NULL  ,
+        lastname          Varchar (255) NOT NULL  ,
+        firstname         Varchar (255) NOT NULL  ,
+        role              Int NOT NULL  ,
+        activation_status Int NOT NULL  ,
+        activation_key    Varchar (255) ,
+        street            Varchar (255) ,
+        zip               Varchar (255) ,
+        city              Varchar (255) ,
+        country           Varchar (255) ,
+        state             Varchar (255) ,
+        locale            Varchar (25) ,
+        token             Varchar (255) ,
+        PRIMARY KEY (id )
 )ENGINE=InnoDB;
+
 
 CREATE TABLE rate(
-        id       Int NOT NULL ,
-        label    Varchar(255) NOT NULL ,
-        amount   Decimal (25,2) NOT NULL ,
-        tax_rate Decimal (25,2) NOT NULL ,
-        id_event Int NOT NULL ,
-        PRIMARY KEY (id)
+        id       Int NOT NULL  ,
+        label    Varchar (255) NOT NULL  ,
+        amount   Decimal (25,2) NOT NULL  ,
+        tax_rate Decimal (25,2) NOT NULL  ,
+        id_event Int NOT NULL  ,
+        PRIMARY KEY (id )
 )ENGINE=InnoDB;
+
 
 CREATE TABLE sequence(
-        name    Varchar(255) NOT NULL ,
-        current Int NOT NULL ,
-        PRIMARY KEY (name)
+        name    Varchar (255) NOT NULL  ,
+        current Int NOT NULL  ,
+        PRIMARY KEY (name )
 )ENGINE=InnoDB;
+
 
 CREATE TABLE bill(
-        id        Int NOT NULL ,
-        created_t Varchar(25) NOT NULL ,
-        total_ht  Decimal (25,2) NOT NULL ,
-        total_tax Decimal (25,2) NOT NULL ,
-        total_ttc Decimal (25,2) NOT NULL ,
-        label     Varchar(255) NOT NULL ,
-        username  Varchar(255) NOT NULL ,
-        address   Varchar(255) NOT NULL ,
-        status    Int NOT NULL ,
-        type      Int NOT NULL ,
-        id_user   Int NOT NULL ,
-        id_event  Int NOT NULL ,
-        PRIMARY KEY (id)
+        id        Int NOT NULL  ,
+        created_t Varchar (25) NOT NULL  ,
+        total_ht  Decimal (25,2) NOT NULL  ,
+        total_tax Decimal (25,2) NOT NULL  ,
+        total_ttc Decimal (25,2) NOT NULL  ,
+        label     Varchar (255) NOT NULL  ,
+        username  Varchar (255) NOT NULL  ,
+        address   Varchar (1024) NOT NULL  ,
+        status    Int NOT NULL  ,
+        type      Int NOT NULL  ,
+        id_user   Int NOT NULL  ,
+        id_event  Int NOT NULL  ,
+        PRIMARY KEY (id )
 )ENGINE=InnoDB;
 
+
 CREATE TABLE item(
-        id                    Int NOT NULL ,
-        event_name            Varchar(255) NOT NULL ,
-        event_rate_name       Varchar(255) NOT NULL ,
-        event_rate_amount     Decimal (25,2) NOT NULL ,
-        event_rate_tax        Decimal (25,2) NOT NULL ,
-        quantity              Int NOT NULL ,
-        total_ht              Decimal (25,2) NOT NULL ,
-        total_tax             Decimal (25,2) NOT NULL ,
-        total_ttc             Decimal (25,2) NOT NULL ,
-        participant_firstname Varchar(255),
-        participant_lastname  Varchar(255),
-        participant_title     Varchar(25),
-        id_bill               Int NOT NULL ,
-        PRIMARY KEY (id)
+        id                    Int NOT NULL  ,
+        event_name            Varchar (255) NOT NULL  ,
+        event_rate_name       Varchar (255) NOT NULL  ,
+        event_rate_amount     Decimal (25,2) NOT NULL  ,
+        event_rate_tax        Decimal (25,2) NOT NULL  ,
+        quantity              Int NOT NULL  ,
+        total_ht              Decimal (25,2) NOT NULL  ,
+        total_tax             Decimal (25,2) NOT NULL  ,
+        total_ttc             Decimal (25,2) NOT NULL  ,
+        participant_firstname Varchar (255) ,
+        participant_lastname  Varchar (255) ,
+        participant_title     Varchar (25) ,
+        id_bill               Int NOT NULL  ,
+        PRIMARY KEY (id )
 )ENGINE=InnoDB;
 
 ALTER TABLE event ADD CONSTRAINT FK_event_id_user FOREIGN KEY (id_user) REFERENCES user(id);
