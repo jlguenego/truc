88cb04977b646904630dd4240ec3137ef0421995
Jean-Louis GUENEGO <jlguenego@gmail.com>
2013-04-11 10:48:56 +0200

    privacy policies.

---
+++ skin/default/privacy.php
@@ -0,0 +1,102 @@
+﻿<div class="legal">
+<div class="legal_title">Privacy Policies</div>
+
+<h1>What information do we collect?</h1>
+<p>
+We collect information from you when you register on our site, place an order,
+subscribe to our newsletter, respond to a survey or fill out a form. 
+</p>
+<p>
+When ordering or registering on our site, as appropriate, you may be asked to enter your: name, e-mail address, mailing address or phone number. You may, however, visit our site anonymously.
+</p>
+<p>
+Google, as a third party vendor, uses cookies to serve ads on your site. Google's use of the DART cookie enables it to serve ads to your users based on their visit to your sites and other sites on the Internet. Users may opt out of the use of the DART cookie by visiting the Google ad and content network privacy policy..
+</p>
+
+<h1>What do we use your information for?</h1>
+<p>
+Any of the information we collect from you may be used in one of the following ways: 
+</p>
+
+<ul>
+	<li>To personalize your experience
+(your information helps us to better respond to your individual needs)</li>
+
+	<li>To improve our website
+(we continually strive to improve our website offerings based on the information and feedback we receive from you)
+	</li>
+	<li>To improve customer service
+(your information helps us to more effectively respond to your customer service requests and support needs)
+	</li>
+	<li>To process transactions
+Your information, whether public or private, will not be sold, exchanged, transferred, or given to any other company for any reason whatsoever, without your consent, other than for the express purpose of delivering the purchased product or service requested.
+	</li>
+	<li>To send periodic emails
+The email address you provide for order processing, will only be used to send you information and updates pertaining to your order.
+	</li>
+</ul>
+
+<h1>How do we protect your information? </h1>
+<p>
+We implement a variety of security measures to maintain the safety of your personal information when you place an order or enter, submit, or access your personal information. 
+</p>
+<p>
+We offer the use of a secure server. All supplied sensitive/credit information is transmitted via Secure Socket Layer (SSL) technology and then encrypted into our Payment gateway providers database only to be accessible by those authorized with special access rights to such systems, and are required to?keep the information confidential.
+</p>
+<p>
+After a transaction, your private information (credit cards, social security numbers, financials, etc.) will not be kept on file for more than 60 days.
+</p>
+
+<h1>Do we use cookies? </h1>
+<p>
+Yes (Cookies are small files that a site or its service provider transfers to your computers hard drive through your Web browser (if you allow) that enables the sites or service providers systems to recognize your browser and capture and remember certain information.
+</p>
+<p>
+Do we disclose any information to outside parties? 
+</p>
+<p>
+We do not sell, trade, or otherwise transfer to outside parties your personally identifiable information. This does not include trusted third parties who assist us in operating our website, conducting our business, or servicing you, so long as those parties agree to keep this information confidential. We may also release your information when we believe release is appropriate to comply with the law, enforce our site policies, or protect ours or others rights, property, or safety. However, non-personally identifiable visitor information may be provided to other parties for marketing, advertising, or other uses.
+</p>
+
+
+<h1>Third party links </h1>
+<p>
+Occasionally, at our discretion, we may include or offer third party products or services on our website. These third party sites have separate and independent privacy policies. We therefore have no responsibility or liability for the content and activities of these linked sites. Nonetheless, we seek to protect the integrity of our site and welcome any feedback about these sites.
+</p>
+<p>
+Childrens Online Privacy Protection Act Compliance 
+</p>
+<p>
+We are in compliance with the requirements of COPPA (Childrens Online Privacy Protection Act), we do not collect any information from anyone under 13 years of age. Our website, products and services are all directed to people who are at least 13 years old or older.
+</p>
+<p>
+
+<h1>Online Privacy Policy Only </h1>
+<p>
+This online privacy policy applies only to information collected through our website and not to information collected offline.
+</p>
+<h1>Terms and Conditions</h1>
+<p>
+Please also visit our Terms and Conditions section establishing the use, disclaimers, and limitations of liability governing the use of our website at /etc/terms.html
+</p>
+<h1>Your Consent </h1>
+<p>
+By using our site, you consent to our websites privacy policy.
+</p>
+
+<h1>Changes to our Privacy Policy </h1>
+<p>
+If we decide to change our privacy policy, we will post those changes on this page. 
+</p>
+<h1>Contacting Us </h1>
+<p>
+If there are any questions regarding this privacy policy you may contact us using the information below. 
+</p>
+<p>
+http://www.event-biller.com
+2 allée du Commandant Charcot
+TORCY 77200
+FRANCE
+contact@event-biller.com
+</p>
+</div>
