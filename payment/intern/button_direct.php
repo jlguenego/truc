c3de055af06ac0e370ac8530f0476aed40e6f6d9
THOMIAS Yannis <thomias.yannis@gmail.com>
2013-04-11 11:10:21 +0200

    61: Query String.

--- payment/intern/button_direct.php
+++ payment/intern/button_direct.php
@@ -5,5 +5,7 @@
 <form action="payment/intern/execute_payment.php" method="get">
 	<input type="hidden" name="item_name" value="<?php echo $devis->label; ?>">
 	<input type="hidden" name="amount" value="<?php echo $devis->total_ttc; ?>">
+	<input type="hidden" name="payment_type" value="direct"/>
+	<input type="hidden" name="currency_code" value="EUR">
 	<input type="submit" value="Pay(direct)"/>
 </form>
--- payment/intern/execute_payment.php
+++ payment/intern/execute_payment.php
@@ -1,11 +1,20 @@
+<?php
+	define('BASE_DIR', '../..');
+	require_once(BASE_DIR."/include/misc.inc");
+	$payment_type = "Complete";
+	if ($_GET["payment_type"] == "authorization") {
+		$payment_type = "Pending";
+	}
+?>
 <html>
 	<head>
 		<title>Execute payment</title>
 	</head>
 	<body>
+		<!-- action=payment_success&tx=2MN52113X70237425&st=Pending&amt=1.20&cc=EUR&cm=&item_number= -->
 		<p><?php print_r($_GET); ?></p>
 		<p>Do you want to proceed?</p>
-		<a href="../../index.php?action=payment_success">Yes</a>
+		<a href="../../index.php?action=payment_success&tx=2MN52113X7023<?php printf("%04d", rand(0, 9999)); ?>&st=<?php echo $payment_type; ?>&amt=<?php echo curr($_GET["amount"]); ?>&cc=<?php echo $_GET["currency_code"]; ?>&cm=&item_number=">Yes</a>
 		<a href="../../index.php?action=payment_cancel">No</a>
 	</body>
 </html>
