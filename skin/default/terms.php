3ed07b062790b3945e389c9e6315b363a28d1187
Jean-Louis GUENEGO <jlguenego@gmail.com>
2013-04-15 12:02:37 +0200

    cgu

--- skin/default/terms.php
+++ skin/default/terms.php
@@ -1,1 +1,151 @@
-terms & condition
+<div class="legal">
+<div class="legal_title">Terms and conditions</div>
+
+
+<h1>Introduction</h1>
+
+<p>
+	These document purpose is to define how the web site event-biller.com services can be used.
+	The web site event-biller.com is the property of the following company,
+	which is based in France: 
+</p>
+<p>
+	<b>JLG Consulting</b><br/>
+	Jean-Louis GUENEGO, Entreprise Individuelle<br/>
+	2 allee du Commandant Charcot<br/>
+	77200 TORCY<br/>
+	SIRET: 439 317 058<br/>
+</p>
+<p>
+	We use the following terminology:
+	<ul>
+		<li><b>"The web site": </b>The web site <a href="http://www.event-biller.com">event-biller.com</a>.</li>
+		<li><b>"Event-Biller.com", "We", "Us", "Ourselves" : </b> JLG Consulting</li>
+		<li><b>"User", "You", "Your" : </b> the person which is accessing the web site,
+			including the organization that this person may represent.</li>
+		<li><b>"the services": </b>All services offered by the web site.</li>
+		<li><b>The event organizer, "organizer": </b>A user that uses the web site to organize one or many events.</li>
+		<li><b>The event buyer: </b>A user that wants to buy or book tickets for an event.</li>
+		<li><b>T&C : Abbreviation for Terms and Conditions (the present document).</b></li>
+	</ul>
+</p>
+<p>
+	The web site is free of charge for being used,
+	but it requires the full acceptance of this present document
+	which acts as a contract between event-biller.com and the user each times accessing the web site.
+</p>
+
+<h1>Modification</h1>
+<p>
+	Event-Biller.com reserves the right, at its sole discretion,
+	to modify or replace any of the terms or conditions of this T&C at any time.
+	It is your responsibility to check this T&C periodically for changes.
+	Your continued use of the Services following the posting of any changes
+	to this T&C constitutes acceptance of those changes.
+	If any change to this T&C is not acceptable to you,
+	your sole remedy is to cease accessing, browsing and otherwise using the Services.
+</p>
+<h1>Choice of Law</h1>
+<p>
+	This T&C and the provision of the Services to you are governed by the laws
+	of the French Republic ("République Française").
+</p>
+<h1>Arbitration</h1>
+<p>
+	Any controversy or claim arising out of or relating to this T&C
+	or the provision of the Services shall be exclusively settled
+	by binding arbitration in accordance with the commercial arbitration rules
+	of the "Tribunal de Commerce de Meaux (77), in France".
+	Any such controversy or claim shall be arbitrated on an individual basis,
+	and shall not be consolidated in any arbitration with any claim or controversy
+	of any other party. The arbitration shall be conducted in France,
+	and judgment on the arbitration award may be entered in any court having jurisdiction thereof. Subject to the foregoing arbitration provision, you and EVENTBRITE agree to submit to the personal jurisdiction of the courts located within the city and county of San Francisco, California. Either you or EVENTBRITE may seek any interim or preliminary relief from a court of competent jurisdiction in San Francisco, California, necessary to protect the rights or property of you or EVENTBRITE (or its agents, suppliers, and subcontractors) pending the completion of arbitration.
+</p>
+
+<h1>General description</h1>
+<p>
+	The purpose of the event-biller.com is to help people,
+	organizations, or companies to reduce the financial risk
+	regarding to the activity of event preparation. In particular it tries to act
+	as a crowdfunding service for events : Organizer promote their event project
+	and if there are enough attendees or funds then they make the event happen.
+</p>
+<h1>Detailed description</h1>
+<p>
+	When accessing the web site, a user can browse
+	the existing events or create its own events.
+</p>
+<p>
+	The web site is equiped with an account design pattern which
+	allows the user to browse while being identified,
+	and within a session.
+	Most of activities like event creation and management,
+	or ticket buying or booking requires the user being identified.
+</p>
+<p>
+	When booking a ticket, the user only gives a payment authorization.
+	The payment will be effectively captured only if the event become confirmed.
+	When buying a ticket, the user gives a payment authorization with immediate capture.
+</p>
+<p>
+	All the information entered in the form when requested must be accurate and genuine.
+</p>
+
+<h1>Event creation</h1>
+<p>
+	When a user (acting as an organizer) creates an event, he must be identified. The user specifies
+	when the event will occur (happening date),
+	when the event will have to be confirmed or cancelled (confirmation date).
+	The organizer specifies the condition that are to be met before the confirmation date
+	to let the Event-Biller.com web site confirm the event.
+	The event is by default planned. It means the organizer
+	has not engaged himself that the event will happen.
+	Buyer can only book tickets (not buy).
+	When the event is confirmed, it engages the organizer to make it happen.
+	After an event is confirmed, all claims from buyer regarding this event must be
+	directly addressed to the organizer.
+</p>
+<h1>Event confirmation or cancellation</h1>
+<p>
+	An event will be confirmed or cancelled by Event-Biller.com
+	at the confirmation date if the expected minimum event amount is reached.
+	It can be cancelled or confirmed by the owner before the confirmation date.
+	
+	An event will be confirmed or cancelled before the confirmation date.
+	If it is confirmed then the booked tickets will be emitted as bought tickets
+	and the matching authorized payment will be captured.
+	If an event is cancelled then all booked tickets are cancelled,
+	and because no payment have been captured, no user needs to be refunded.
+</p>
+
+<h1>Identification</h1>
+<p>
+	The user must enter honest information about himself or herself.
+</p>
+<h1>Refund</h1>
+<p>
+	It is the responsibility of the Organizer to communicate its refund policy
+	to Buyers and to issue refunds to Buyers via the Site or otherwise.
+	
+	If a Buyer desires to request a refund,
+	Buyer must request the refund from the Organizer.
+	
+	All communications or disputes regarding refunds are between
+	the Organizer and Buyer and Event-Biller.com will not be responsible
+	or liable in any way for refunds, errors in issuing refunds,
+	or lack of refunds in connection with the Services.
+	
+	All communications and disputes regarding chargebacks
+	and refunds are between the Organizer and Buyer and Event-Biller.com will not
+	be responsible or liable in any way for chargebacks in connection with a Buyer's use
+	of the Services.
+	
+	If you are a Buyer and you wish to request a refund in connection
+	with an event listed on the Services, you should contact the applicable Organizer directly.
+</p>
+
+<h1>Security</h1>
+<p>
+	If needed, the user can access the web site with an HTTPS access.
+	The certificate is owned by the Event-Biller.com web hosting provider, which is OVH SAS.
+</p>
