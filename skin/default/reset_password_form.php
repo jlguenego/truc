9c27045be7f07e2980ff1e88f775417c86f8d16d
THOMIAS Yannis <thomias.yannis@gmail.com>
2013-04-18 16:23:54 +0200

    Supervision translation.

--- skin/default/reset_password_form.php
+++ skin/default/reset_password_form.php
@@ -1,13 +1,13 @@
 <?php
 	$f = new Form();
-	$f->title = "Reset password";
+	$f->title = "{{Reset password}}";
 	$f->action = "?action=handle_reset_password";
 	$f->method = "POST";
-	$f->add_password("New password", "clear_password1", "Choose a new password");
-	$f->add_password("Retype new password", "clear_password2", "Confirm the new password");
+	$f->add_password(_t("New password"), "clear_password1", _t("Choose a new password"));
+	$f->add_password(_t("Retype new password"), "clear_password2", _t("Confirm the new password"));
 	$f->add_hidden("password1", "");
 	$f->add_hidden("password2", "");
-	$f->add_submit("Reset password");
+	$f->add_submit(_t("Reset password"));
 	echo $f->html();
 ?>
 <script>
--- skin/default/supervision.php
+++ skin/default/supervision.php
@@ -6,12 +6,12 @@
 
 <table class="evt_table">
 	<tr>
-		<th>Event ID</th>
-		<th>Event name</th>
-		<th>Event date</th>
-		<th>Confirmation date</th>
-		<th>Status</th>
-		<th>Publish flag</th>
+		<th>{{Event ID}}</th>
+		<th>{{Event name}}</th>
+		<th>{{Event date}}</th>
+		<th>{{Confirmation date}}</th>
+		<th>{{Status}}</th>
+		<th>{{Publish flag}}</th>
 	</tr>
 <?php
 		foreach ($g_display["events"] as $event) {
@@ -24,7 +24,7 @@
 		<td><?php echo $event->happening_t; ?></td>
 		<td><?php echo $event->confirmation_t; ?></td>
 		<td><?php echo $event->display_status(); ?></td>
-		<td><?php echo $publish_flag; ?></td>
+		<td><?php echo _t($publish_flag); ?></td>
 <?php
 			$confirm_button_grey = "";
 			$cancel_button_grey = "";
@@ -42,7 +42,7 @@
 ?>
 		<td>
 			<form action="?action=publish_event&amp;id=<?php echo $event->id ?>" method="POST">
-				<input type="submit" value="Publish event" <?php echo $publish_button_grey ?>/>
+				<input type="submit" value="{{Publish event}}" <?php echo $publish_button_grey ?>/>
 			</form>
 		</td>
 <?php
@@ -50,9 +50,12 @@
 ?>
 		<td>
 			<form name="unpublish" action="?action=unpublish_event&amp;id=<?php echo $event->id ?>" method="POST">
-				<input type="submit" value="Unpublish event" <?php echo $publish_button_grey ?>/>
+				<input type="submit" value="{{Unpublish event}}" <?php echo $publish_button_grey ?>/>
 				<input type="hidden" name="reason" />
 			</form>
+			<div id="dialog" style="display: none;" title="Unpublish reason">
+				<textarea id="dialog_textarea">The reason is ...</textarea>
+			</div>
 		</td>
 <?php
 				}
@@ -60,12 +63,12 @@
 ?>
 		<td>
 			<form action="?action=confirm_event&amp;id=<?php echo $event->id ?>" method="POST">
-				<input type="submit" value="Confirm event" <?php echo $confirm_button_grey ?>/>
+				<input type="submit" value="{{Confirm event}}" <?php echo $confirm_button_grey ?>/>
 			</form>
 		</td>
 		<td>
 			<form action="?action=cancel_event&amp;id=<?php echo $event->id ?>" method="POST">
-				<input type="submit" value="Cancel event" <?php echo $cancel_button_grey ?>/>
+				<input type="submit" value="{{Cancel event}}" <?php echo $cancel_button_grey ?>/>
 			</form>
 		</td>
 	</tr>
@@ -73,9 +76,6 @@
 		}
 ?>
 </table>
-<div id="dialog" style="display: none;" title="Unpublish reason">
-	<textarea id="dialog_textarea">The reason is ...</textarea>
-</div>
 <script>
 	$(document).ready(eb_unpublish);
 </script>
