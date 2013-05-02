62ba07d2957fa544c2b7abda723d6fec1a26d2f4
THOMIAS Yannis <thomias.yannis@gmail.com>
2013-05-02 14:31:15 +0200

    d5

--- skin/default/root.php
+++ skin/default/root.php
@@ -1,5 +1,22 @@
 	<div id="evt_menu">
-		<a class="evt_button" href="?action=get_form&amp;type=event">{{Create an event}}</a>
+		<p>
+			<b>Organize your event without risk.</b>
+		</p>
+		<h1>Be sure people will support you!</h1>
+		<p>
+			Event-Biller is a <b>crowdfunding</b> platform that allows you to try to
+			organize events.
+		</p>
+		<p>
+			If enough attendees have booked tickets for your event and made payment
+			authorization, then you can:<br/>
+			<i>
+				- Confirm the event<br/>
+				- Capture all the payments<br/>
+			</i>
+			So you have both the <b>money</b> and the <b>attendees</b> to successfully make your event.
+		</p>
+		<a class="evt_button" href="?action=get_form&amp;type=event">{{Declare an event}}</a>
 		<a class="evt_button" href="events">{{See events}}</a>
 <?php
 	// Verify if user is logged in to show differents menu.
