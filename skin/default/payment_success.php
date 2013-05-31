3546b1ea232599cf8c96f965d04468fc733d2a5a
THOMIAS Yannis <thomias.yannis@gmail.com>
2013-05-31 11:28:12 +0200

    Add "back to event" link

--- skin/default/payment_success.php
+++ skin/default/payment_success.php
@@ -13,4 +13,6 @@
 <span class="evt_title"><p>{{Payment success!}}</p></span>
 {{The <?php echo $payment_html; ?> succeeded. We just sent a mail to you for details.}}<br/>
 <br/>
-{{You can access to this quotation via this permalink:}} <a href="<?php echo $link; ?>" target="_blank"><?php echo $link; ?></a>.
+{{You can access to this quotation via this permalink:}} <a href="<?php echo $link; ?>" target="_blank"><?php echo $link; ?></a>.<br/>
+<br/>
+{{Back to the}} <a href="<?php echo "event/".$devis->event_id; ?>">{{event page}}</a>
