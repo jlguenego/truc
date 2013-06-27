3275d60b38f3013549f0f27faf69081b6ce3ce97
THOMIAS Yannis <thomias.yannis@gmail.com>
2013-06-27 16:34:45 +0200

    Upgrade JQuery.UI to 1.10.3

--- skin/default/layout.php
+++ skin/default/layout.php
@@ -16,9 +16,9 @@
 		<script type="text/javascript" src="jscript/misc.js"></script>
 		<link href="<?php echo SKIN_DIR; ?>/default.css" rel="stylesheet">
 
-		<link href="_ext/jquery-ui-1.10.1.custom/css/ui-lightness/jquery-ui-1.10.1.custom.css" rel="stylesheet">
-		<script src="_ext/jquery-ui-1.10.1.custom/js/jquery-1.9.1.js"></script>
-		<script src="_ext/jquery-ui-1.10.1.custom/js/jquery-ui-1.10.1.custom.js"></script>
+		<link href="_ext/jquery-ui-1.10.3.custom/css/ui-lightness/jquery-ui-1.10.3.custom.css" rel="stylesheet">
+		<script src="_ext/jquery-ui-1.10.3.custom/js/jquery-1.9.1.js"></script>
+		<script src="_ext/jquery-ui-1.10.3.custom/js/jquery-ui-1.10.3.custom.js"></script>
 		<script src="_ext/tinymce/tinymce.min.js"></script>
 		<script src="_ext/sha1.js"></script>
 		<link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Open+Sans">
